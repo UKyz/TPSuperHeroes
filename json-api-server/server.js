a5f2fd18588b962b292553f078e17592bf2a13b2
UKyz <ukyz@icloud.com>
2018-10-29 22:33:56 +0100

    Add server

---
+++ json-api-server/server.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+const app = require('express')();
+const API = require('json-api');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+if (!process.env.BASE_URL || !process.env.SUFFIX_URL || !process.env.PORT) {
+  console.log('You should check if your environment variables are good.');
+  process.exit(1);
+}
+
+const dbURL = process.env.DB || 'mongodb://127.0.0.1:27017/dev-yahoo-finance';
+
+const APIError = API.types.Error;
+mongoose.connect(dbURL, {useMongoClient: true});
+
+const models = {
+  Author: require('./models/author').model,
+  Book: require('./models/book').model
+};
+
+const registryTemplates = {
+  authors: require('./models/author').registry,
+  books: require('./models/book').registry
+};
+
+const adapter = new API.dbAdapters.Mongoose(models);
+const registry = new API.ResourceTypeRegistry(registryTemplates,
+  {dbAdapter: adapter});
+
+const docs = new API.controllers.Documentation(registry, {name: 'Stock API'});
+const controller = new API.controllers.API(registry);
+const front = new API.httpStrategies.Express(controller, docs);
+
+const apiReqHandler = front.apiRequest.bind(front);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Cache-Control');
+  res.header('Access-Control-Allow-Methods',
+    'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+const db = [
+  'books',
+  'authors'
+];
+
+app.options('*', (req, res) => {
+  res.send();
+});
+
+app.get('/api', front.docsRequest.bind(front));
+
+app.route(`/api/:type(${db.join('|')})`).get(apiReqHandler).post(apiReqHandler)
+  .patch(apiReqHandler);
+
+app.route(`/api/:type(${db.join('|')})/:id`).get(apiReqHandler)
+  .patch(apiReqHandler)
+  .delete(apiReqHandler);
+
+app.route(`/api/:type(${db.join('|')})/:id/relationships/:relationship`)
+  .get(apiReqHandler).post(apiReqHandler).patch(apiReqHandler)
+  .delete(apiReqHandler);
+
+app.use((req, res) => {
+  front.sendError(new APIError(404, undefined, 'Not Found'), req, res);
+});
+
+app.listen(process.env.PORT);
