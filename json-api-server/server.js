a042d4d6cf092d6fa1bafb9b1c444652344a2c3b
UKyz <ukyz@icloud.com>
2018-11-03 19:27:43 +0100

    Add test.js

--- json-api-server/server.js
+++ json-api-server/server.js
@@ -1,77 +1,3 @@
-#!/usr/bin/env node
-/*const app = require('express')();
-const API = require('json-api');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-if (!process.env.BASE_URL || !process.env.SUFFIX_URL || !process.env.PORT) {
-  console.log('You should check if your environment variables are good.');
-  process.exit(1);
-}
-
-const dbURL = process.env.DB || 'mongodb://127.0.0.1:27017/dev-yahoo-finance';
-
-const APIError = API.types.Error;
-mongoose.connect(dbURL, {useMongoClient: true});
-
-const models = {
-  Hero: require('./models/hero').model,
-  Villain: require('./models/villain').model
-};
-
-const registryTemplates = {
-  heroes: require('./models/hero').registry,
-  villains: require('./models/villain').registry
-};
-
-const adapter = new API.dbAdapters.Mongoose(models);
-const registry = new API.ResourceTypeRegistry(registryTemplates,
-  {dbAdapter: adapter});
-
-const docs = new API.controllers.Documentation(registry, {name: 'Stock API'});
-const controller = new API.controllers.API(registry);
-const front = new API.httpStrategies.Express(controller, docs);
-
-const apiReqHandler = front.apiRequest.bind(front);
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Cache-Control');
-  res.header('Access-Control-Allow-Methods',
-    'POST, GET, PATCH, DELETE, OPTIONS');
-  next();
-});
-
-const db = [
-  'heroes',
-  'villains'
-];
-
-app.options('*', (req, res) => {
-  res.send();
-});
-
-app.get('/api', front.docsRequest.bind(front));
-
-app.route(`/api/:type(${db.join('|')})`).get(apiReqHandler).post(apiReqHandler)
-  .patch(apiReqHandler);
-
-app.route(`/api/:type(${db.join('|')})/:id`).get(apiReqHandler)
-  .patch(apiReqHandler)
-  .delete(apiReqHandler);
-
-app.route(`/api/:type(${db.join('|')})/:id/relationships/:relationship`)
-  .get(apiReqHandler).post(apiReqHandler).patch(apiReqHandler)
-  .delete(apiReqHandler);
-
-app.use((req, res) => {
-  front.sendError(new APIError(404, undefined, 'Not Found'), req, res);
-});
-
-
-app.listen(process.env.PORT);*/
-
 /* ---- HTTP ---- */
 
 const http = require('http');
@@ -84,39 +10,11 @@ const div_ = (a, b) => a / b;
 
 const nts = (fn, x, y) => String(fn(Number(x), Number(y)));
 
-/*const server = http.createServer((req, res) => {
- const urlRequest = url.parse(req.url, true, true);
-
- if (urlRequest.pathname === '/add') {
- res.end(nts(add_, urlRequest.query.var1, urlRequest.query.var2));
- } else if (urlRequest.pathname === '/sub') {
- res.end(nts(sub_, urlRequest.query.var1, urlRequest.query.var2));
- } else if (urlRequest.pathname === '/mul') {
- res.end(nts(mul_, urlRequest.query.var1, urlRequest.query.var2));
- } else if (urlRequest.pathname === '/div') {
- res.end(nts(div_, urlRequest.query.var1, urlRequest.query.var2));
- }
- res.end('404');
- });
-
- server.listen(3000);*/
-
 /* ---- express ---- */
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-/*mongoose.connect(process.env.DB || 'mongodb://localhost:27017/deco-test',
-  {useNewUrlParser: true});
-
-const blobSchema = new mongoose.Schema({
-  blob: Object
-});
-
-const Blob = mongoose.model('Blob', blobSchema);
-const app = express();
-app.use(bodyParser.json());*/
 
 app.get('/add', (req, res) => {
   res.status(200).send({result : nts(add_, req.query.var1, req.query.var2)});
