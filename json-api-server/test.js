b7fa07f0f095c1816a30a64859ad270ee0197e53
UKyz <ukyz@icloud.com>
2018-11-04 22:06:31 +0100

    Update test

--- json-api-server/test.js
+++ json-api-server/test.js
@@ -1,4 +1,8 @@
 const rp = require('request-promise');
 
-rp('http://localhost:1337/add?var1=2&var2=3')
-  .then((body) => {console.log(body)});
+const add = (var1, var2) =>{
+  rp(`http://localhost:1337/add?var1=${var1}&var2=${var2}`)
+    .then((body) => {console.log(body)});
+};
+
+add(2, 3);
