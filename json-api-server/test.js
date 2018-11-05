00ac1d7a9da108fc5aba9f2e4e5388612a681cc9
UKyz <ukyz@icloud.com>
2018-11-05 09:22:46 +0100

    Delete tests

--- json-api-server/test.js
+++
@@ -1,8 +0,0 @@
-const rp = require('request-promise');
-
-const add = (var1, var2) =>{
-  rp(`http://localhost:1337/add?var1=${var1}&var2=${var2}`)
-    .then((body) => {console.log(body)});
-};
-
-add(2, 3);
--- json-api-server2/test.js
+++
@@ -1,8 +0,0 @@
-const rp = require('request-promise');
-
-const add = (var1, var2) =>{
-  rp(`http://localhost:1338/add?var1=${var1}&var2=${var2}`)
-    .then((body) => {console.log(body)});
-};
-
-add(2, 3);
