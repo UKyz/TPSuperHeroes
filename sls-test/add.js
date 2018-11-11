ec6a2da56bb99af88eab9bd7f4e66368dac35428
UKyz <ukyz@icloud.com>
2018-11-11 16:11:43 +0100

    Fix dkc and async xo problem

--- sls-test/add.js
+++ sls-test/add.js
@@ -1,6 +1,7 @@
 const add = ({a, b}) => a + b;
 
-const addHandler = msg => ({
+/* eslint-disable-next-line require-await */
+const addHandler = async msg => ({
   status: 200,
   body: JSON.stringify(add(JSON.parse(msg.body)))
 });
