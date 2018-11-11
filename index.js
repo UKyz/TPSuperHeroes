a25833b8380469d0915d0b765b44a55a870f4aa4
UKyz <ukyz@icloud.com>
2018-11-11 15:51:41 +0100

    Add sls-test, update dkc

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const rp = require('request-promise');
 
 const newHero = (name, age) => {
-  rp(`http://localhost:1338/createNewHero?name=${name}&age=${age}`);
+  rp(`http://localhost:3020/createNewHero?name=${name}&age=${age}`);
 };
 
 const newVillain = (name, age) => {
-  rp(`http://localhost:1337/createNewVillain?name=${name}&age=${age}`);
+  rp(`http://localhost:3010/createNewVillain?name=${name}&age=${age}`);
 };
 
 const getVillains = () => {
-  rp({uri: 'http://localhost:1337/getListVillains', json: true})
+  rp({uri: 'http://localhost:3010/getListVillains', json: true})
     .then(body => {
       console.log(body.result);
     });
 };
 
 const getHeroes = () => {
-  rp({uri: 'http://localhost:1338/getListHeroes', json: true})
+  rp({uri: 'http://localhost:3020/getListHeroes', json: true})
     .then(body => {
       console.log(body.result);
     });
