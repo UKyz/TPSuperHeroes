89af181d9e441bdc13e6d90095b33fc0e1cd7d5a
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2018-12-26 08:56:11 +0100

    Added hero getter for the front
    A function had to be rewritten to be able to retrieve the information necessary for the proper functioning of the front end

---
+++ sls-hero/src/get-heroes-front.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const R = require('ramda');
+
+const {hookHero} = require('./hook-hero');
+
+const getHero = hero => ({
+  name: hero.name_,
+  pos: hero.pos_,
+  speed: hero.speed_,
+  score: hero.score_,
+  moving: hero.moving_
+});
+
+const filterPropsHeroes = list => R.map(getHero, list);
+
+const getHeroes = async () => {
+  mongoose.connect(process.env.DB, {useNewUrlParser: true});
+  const heroSchema = require('../model/hero').schema;
+  const HeroModel = mongoose.model('heroModel', heroSchema);
+  let heroes = await HeroModel.find({}).exec();
+  heroes.forEach(async hero => {
+    await hookHero(hero._id);
+  });
+  heroes = await HeroModel.find({moving_: false}).exec();
+  return filterPropsHeroes(heroes);
+};
+
+const getHeroesAvailableHandlerForFront = async () => ({
+  status: 200,
+  body: JSON.stringify(await getHeroes())
+});
+
+module.exports = {
+  getHeroesAvailableHandlerForFront
+};
